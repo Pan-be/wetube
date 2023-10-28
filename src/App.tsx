1fb97a835977a52c4fadde1d12cf831da1a75079
Konrad Bieniek <konradbieniek@MacBook-Air-Konrad.local>
2023-10-28 22:23:36 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='p-0 max-h-screen flex flex-col items-start'>
+      <div>1</div>
+      <div>2</div>
+    </div>
+  )
+}
+
+export default App
