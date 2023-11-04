9c831a6d7712de954281ef93e4dd281b15ce5a88
Konrad Bieniek <konradbieniek@MacBook-Air-Konrad.local>
2023-11-04 22:42:07 +0100

    top scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
+import { CategoryPills } from './components/CategoryPills.tsx'
+import { categories } from './data/home.ts'
 import { PageHeader } from './layouts/PageHeader.tsx'
 
 function App() {
-
+  const [selectedCategory, setSelectedCategory] = useState(categories[0])
   return (
-    <div className="w-screen p-0 max-h-screen flex flex-col items-start">
+    <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div>2</div>
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div className="">Sidebar</div><div className="overflow-x-hidden px-8 py-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills categories={categories}
+              selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
+          </div></div>
+      </div>
     </div>
   )
 }
---
+++ src/components/CategoryPills.tsx
@@ -0,0 +1,93 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Button } from "./Button";
+import { useEffect, useRef, useState } from "react";
+
+type CategoryPillProps = {
+    categories: string[],
+    selectedCategory: string,
+    onSelect: (category: string) => void
+}
+
+const TRANSLATE_AMOUNT = 200
+
+export function CategoryPills({ categories, selectedCategory, onSelect }: CategoryPillProps) {
+
+    const [translate, setTranslate] = useState(0)
+    const [isLeft, setIsLeft] = useState(true)
+    const [isRight, setIsRight] = useState(true)
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (containerRef.current == null) return
+
+        const observer = new ResizeObserver(entries => {
+
+            const container = containerRef.current
+
+            if (container == null) return
+            setIsLeft(translate > 0)
+            setIsRight(translate + container.clientWidth < container.scrollWidth)
+        })
+        observer.observe(containerRef.current)
+
+        return () => {
+            observer.disconnect()
+        }
+
+    }, [categories, translate])
+
+    return <div className="overflow-x-hidden relative"
+        ref={containerRef}>
+        <div
+            className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]"
+            style={{ transform: `translateX(-${translate}px)` }}>
+            {categories.map(category => (
+                <Button
+                    key={category}
+                    onClick={() => onSelect(category)}
+                    variant={selectedCategory === category ? "dark" : "default"}
+                    className="py-1 px-3 rounded-lg whitespace-nowrap">
+                    {category}
+                </Button>))}
+        </div>
+        {isLeft && (
+            <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to transparent w-24 h-full">
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    className="h-full w-auto aspect-square p-1.5"
+                    onClick={() => {
+                        setTranslate(translate => {
+                            const newTranslate = translate - TRANSLATE_AMOUNT
+                            if (newTranslate <= 0) return 0
+                            return newTranslate
+                        })
+                    }}>
+                    <ChevronLeft />
+                </Button>
+            </div>)}
+        {isRight && (
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to transparent w-24 h-full">
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    className="h-full w-auto aspect-square p-1.5 flex justify-end"
+                    onClick={() => {
+                        setTranslate(translate => {
+                            if (containerRef.current === null) {
+                                return translate
+                            }
+                            const newTranslate = translate + TRANSLATE_AMOUNT
+                            const edge = containerRef.current.scrollWidth
+                            const width = containerRef.current.clientWidth
+                            if (newTranslate + width >= edge) {
+                                return edge - width
+                            }
+                            return newTranslate
+                        })
+                    }}>
+                    <ChevronRight />
+                </Button>
+            </div>)}
+    </div>
+}
