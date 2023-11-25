4902aa524446dc9793f101e1cd7533326bdc7109
Konrad Bieniek <konradbieniek@MacBook-Air-Konrad.local>
2023-11-25 01:10:15 +0100

    changes

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -30,15 +30,20 @@ export function VideoGridItem({ id, title, channel, views, postedAt, duration, t
         if (isVideoPlaying) {
             videoRef.current.currentTime = 0
             videoRef.current.play()
+        } else {
+            videoRef.current.pause()
         }
+
+
     }, [isVideoPlaying])
 
-    return <div className="flex flex-col gap-2">
+    return <div className="flex flex-col gap-2" onMouseEnter={() => setIsVideoPlaying(true)} onMouseLeave={() => setIsVideoPlaying(false)}>
         <a href={`/watch?v=${id}`} className='relative aspect-video'>
-            <img src={thumbnailUrl} alt="video miniature" className="block w-full h-full object-cover rounded-xl" />
+            <img src={thumbnailUrl} alt="video miniature" className={`block w-full h-full object-cover transition-[border-radius] duration-200 ${isVideoPlaying ? 'rounded-none' : 'rounded-xl'}`} />
             <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
                 {formatDuration(duration)}
             </div>
+            <video className={`block h-full object-cover absolute inset-0 transition-opacity duration-200 ${isVideoPlaying ? "opacity-100 delay-200" : "opacity-0"}`} ref={videoRef} muted playsInline src={videoUrl}></video>
         </a>
         <div className="flex gap-2">
             <a href={`/@${channel.id}`} className="flex-shrink-0">
